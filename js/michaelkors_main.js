fdf1cdcd5cb2e2f7ca33a38453e40bd4c3a28e31
wnsskdud <wnsskdud@naver.com>
2022-05-12 10:19:02 +0900

    S

--- js/michaelkors_main.js
+++ js/michaelkors_main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 $(function(){
     var btn = $('.cnt01>div.btn');
     var artGroup = $('.artGroup');
@@ -56,63 +55,4 @@ $(function(){
     //     }
     //     $('.cnt01 div.artGroup').css('margin-left',i*-100+'%');
     // });
-=======
-$(function(){
-    var btn = $('.cnt01>div.btn');
-    var artGroup = $('.artGroup');
-    var wd = $(window).width();
-    var mvLeft = 0;
-    var mvRight = 0;
-
-    if(wd > 1024){
-        mvLeft = 25;
-        mvRight = 50;
-    }else if(wd >= 420 && wd <= 1024){
-        mvLeft = 33;
-        mvRight = 66;
-    }else{
-        mvLeft = 50;
-        mvRight = 100;
-    }
-
-    artGroup.find('.article:last').prependTo(artGroup);
-    artGroup.css({
-        marginLeft : -mvLeft+'%'
-    });
-    btn.find('i').first().click(function(){
-        artGroup.stop().animate({
-            marginLeft : '0%'
-        },400,function(){
-            artGroup.find('.article:last').prependTo(artGroup);
-            artGroup.css({
-                marginLeft : -mvLeft+'%'
-            });
-        });
-    });
-    btn.find('i').last().click(function(){
-        artGroup.stop().animate({
-            marginLeft : -mvRight+'%'
-        },400,function(){
-            artGroup.find('.article:first').appendTo(artGroup);
-            artGroup.css({
-                marginLeft : -mvLeft+'%'
-            });
-        });
-    });
-    // var i = 0;
-    // $('.cnt01>div.btn>i').eq(0).click(function(){
-    //     i--;
-    //     if(i <= 0){
-    //         i = 0;
-    //     }
-    //     $('.cnt01 div.artGroup').css('margin-left',i*-100+'%');
-    // });
-    // $('.cnt01>div.btn>i').eq(1).click(function(){
-    //     i++;
-    //     if(i >= 3){
-    //         i = 2;
-    //     }
-    //     $('.cnt01 div.artGroup').css('margin-left',i*-100+'%');
-    // });
->>>>>>> 7839adb27e9f6d4e360128162e6d51334055d781
 });
--- js/michaelkors_sub1.js
+++ js/michaelkors_sub1.js
@@ -1,21 +1,3 @@
-
-$(function(){
-    var btn = $('.main ul.tab');
-    $(window).scroll(function(){
-        console.log(window);
-    });
-
-    btn.find('li:first').click(function(){
-        $('html,body').stop().animate({
-            scrollTop: 800
-        },500);
-    });
-    btn.find('li:last').click(function(){
-        $('html,body').stop().animate({
-            scrollTop: 2700
-        },700);
-    });
-
 $(function(){
     var btn = $('.main ul.tab');
     $(window).scroll(function(){
